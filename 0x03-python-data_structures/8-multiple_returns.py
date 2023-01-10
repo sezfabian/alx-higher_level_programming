91ead12869206e8ff97b1302f01095ddb0451dcc
sezfabian <cheruiyotfabian@gmail.com>
2023-01-10 15:03:24 +0300

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        tuple_n = (0, 'none')
+        tuple_n = (0, None)
         return tuple_n
     else:
         tuple_n = (len(sentence), sentence[0])
