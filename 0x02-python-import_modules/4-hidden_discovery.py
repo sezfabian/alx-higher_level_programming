0b70252a6c71b4b1c94c0da168f159609bc4950a
sezfabian <cheruiyotfabian@gmail.com>
2023-01-13 00:11:18 +0300

    task 04m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if not i.startswith('__'):
-            print(i)
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
