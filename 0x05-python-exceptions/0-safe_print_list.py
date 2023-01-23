5bd0d17a441dfc078bfd1e85d709a4bd90ff1c8f
sezfabian <cheruiyotfabian@gmail.com>
2023-01-23 19:00:27 +0300

    task 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             count += 1
         except IndexError:
-            print()
-            return count
+            break
     print()
     return count
