184e0cdf56b167d0e6c44bbbf1116bf61cda222b
sezfabian <cheruiyotfabian@gmail.com>
2023-01-31 23:44:00 +0300

    task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
-#!/usr/bin/python3
-"""magic_string module"""
-
-
 def magic_string():
-    """returns a string “BestSchool” n times the number of the iteration """
-    mystr = ""
     magic_string.counter += 1
-    if magic_string.counter == 1:
-        return "BestSchool"
-    mystr = "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
-    return mystr
-
-
+    return "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
 magic_string.counter = 0
