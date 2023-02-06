9cec4bbdda587a01d7f6d3edb7b3a8896db7ca0d
sezfabian <cheruiyotfabian@gmail.com>
2023-02-06 23:17:23 +0300

    h06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""5-base_geometry module"""
+
+
+class BaseGeometry:
+    """new Class BaseGeometry"""
+
+    def area(self):
+        """Public instance method: def area(self): that raises an Exception"""
+        raise Exception("area() is not implemented")
