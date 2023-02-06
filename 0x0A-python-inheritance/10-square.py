8f0db3ed51f68e527aaa9022f30a8b3b5c3df3c1
sezfabian <cheruiyotfabian@gmail.com>
2023-02-07 02:04:16 +0300

    h10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""10-square module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle (9-rectangle.py)"""
+
+    def __init__(self, size):
+        """initializes object square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
