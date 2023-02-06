61ac162583d87cd3d8ab48a44d96c8ed8bcac71f
sezfabian <cheruiyotfabian@gmail.com>
2023-02-06 21:05:26 +0300

    h03

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""2-is_same_class module"""
+
+
+def is_same_class(obj, a_clas):
+    """object is exactly an instance of the specified class"""
+    return (type(obj) == a_clas)
