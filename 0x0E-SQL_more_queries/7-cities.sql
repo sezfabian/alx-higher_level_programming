07752b1d890dfd1617c0bac84790d6b19b5bacbd
sezfabian <cheruiyotfabian@gmail.com>
2023-03-18 00:20:57 +0300

    Foreign Key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,10 +6,11 @@
 --      If the database hbtn_0d_usa already exists, your script should not fail
 --      If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities(
-  id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
   state_id INT NOT NULL,
   FOREIGN KEY(state_id) REFERENCES states(id),
   name VARCHAR(256) NOT NULL,
-);
+  );
+
