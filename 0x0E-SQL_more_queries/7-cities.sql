f6a5b5f8442ca21d7f14d05e4a736c02923e04db
sezfabian <cheruiyotfabian@gmail.com>
2023-03-18 00:23:38 +0300

    new'

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -10,7 +10,7 @@ USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities(
   id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
   state_id INT NOT NULL,
-  FOREIGN KEY(state_id) REFERENCES states(id),
+  FOREIGN KEY(`state_id`) REFERENCES states(`id`),
   name VARCHAR(256) NOT NULL,
   );
 
