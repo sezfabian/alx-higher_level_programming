82ce1155dabf72682dbf6b937c2e202b48750877
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-15 10:54:51 +0300

    java8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,11 @@ const myNum = parseInt(process.argv[2]);
 if (isNaN(myNum)) {
   console.log('Missing size');
 } else {
-  let mystring = '';
   for (let i = 1; i <= process.argv[2]; i++) {
+    let mystring = '';
     for (let i = 1; i <= process.argv[2]; i++) {
       mystring += '#';
     }
-    mystring += '\n';
+    console.log(mystring);
   }
-  console.log(mystring);
 }
