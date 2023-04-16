1151ef2bc51dfa0cc9f8ecff7793c870ae5421c6
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-16 10:47:22 +0300

    java11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const argSize = process.argv.length;
 let maxNum = 0;
 let secNum = 0;
 for (let i = 2; i < argSize; i++) {
-  if (parseInt(process.argv[i]) > maxNum) {
+  if (parseInt(process.argv[i]) > maxNum && argSize > 3) {
     secNum = maxNum;
     maxNum = parseInt(process.argv[i]);
   }
