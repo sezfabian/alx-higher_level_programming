d4a6f0757eb76d7399cb9c6aea3c016ad583a205
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-16 10:44:41 +0300

    java11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const argSize = process.argv.length;
+let maxNum = 0;
+let secNum = 0;
+for (let i = 2; i < argSize; i++) {
+  if (parseInt(process.argv[i]) > maxNum) {
+    secNum = maxNum;
+    maxNum = parseInt(process.argv[i]);
+  }
+}
+console.log(secNum);
