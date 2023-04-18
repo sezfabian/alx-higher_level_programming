7b8bbc2486cc2fbe120f7c595759e4f6a4192295
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-18 12:45:31 +0300

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, \
+                    cities.state_id, states.name \
+                    FROM cities \
+                    INNER JOIN states ON cities.state_id = states.id \
+                    ORDER BY cities.id ASC")
+    myStr = ""
+    for c in cursor.fetchall():
+        if c[3] == sys.argv[4]:
+            myStr += c[1] + ', '
+    print(myStr[:-2])
