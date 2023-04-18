d3767e907e883066715cc8e7cc2ace24698cc406
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-18 03:45:14 +0300

    safefilter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %(username)s \
+        ORDER BY id ASC", {'username': sys.argv[4]})
+    for state in cursor.fetchall():
+        if state[1] == sys.argv[4]:
+            print(state)
