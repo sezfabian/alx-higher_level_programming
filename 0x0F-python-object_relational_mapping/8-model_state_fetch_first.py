f18b9992b80594036582581bbf1fa639356e2ed8
Fabian Cheruiyot <sezfabian@localhost.localdomain>
2023-04-20 10:44:03 +0300

    task8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+Usage: ./7-model_state_fetch_all.py <mysql username> /
+                                    <mysql password> /
+                                    <database name>
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+from model_state import State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).first()
+    print("{}: {}".format(result.id, result.name))
