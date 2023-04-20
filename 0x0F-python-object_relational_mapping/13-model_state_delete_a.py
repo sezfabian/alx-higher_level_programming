988de6282a08390577950866f55ebc1fc886ee48
sezfabian <cheruiyotfabian@gmail.com>
2023-04-20 13:03:44 +0300

    deletes

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
+deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
 Usage: ./13-model_state_delete_a.py <mysql username> /
                                     <mysql password> /
                                     <database name>
