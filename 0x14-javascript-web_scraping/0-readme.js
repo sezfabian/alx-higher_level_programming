7a97b52068d705e532e2f10a1af8868d2931bd91
sezfabian <cheruiyotfabian@gmail.com>
2023-05-24 01:48:32 +0300

    readfile

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+const filepath = process.argv[2];
+
+fs.readFile(filepath, 'utf8', (error, data) => {
+    if (data) {
+        console.log(data);
+    }
+    if (error) {
+        console.log(error);
+    }
+    });
