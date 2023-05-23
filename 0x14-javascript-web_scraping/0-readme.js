3bb1923f3a6fc14850f1f79d253c3defba2d1a24
sezfabian <cheruiyotfabian@gmail.com>
2023-05-24 01:50:05 +0300

    standard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const process = require('process');
 const filepath = process.argv[2];
 
 fs.readFile(filepath, 'utf8', (error, data) => {
-    if (data) {
-        console.log(data);
-    }
-    if (error) {
-        console.log(error);
-    }
-    });
+  if (data) {
+    console.log(data);
+  }
+  if (error) {
+    console.log(error);
+  }
+});
