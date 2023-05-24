22a6340473077ad7f6b870d61c32e48111eaf11e
sezfabian <cheruiyotfabian@gmail.com>
2023-05-24 03:56:31 +0300

    characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,14 +3,14 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request.get(url, (err, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
     for (const character of JSON.parse(body).characters) {
-      request.get(character, (err, response, body) => {
-        if (err) {
-          console.log(err);
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
         } else if (response.statusCode === 200) {
           console.log(JSON.parse(body).name);
         }
